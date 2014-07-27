9de3888e01b03cf3fe17fcf4fe4aa42b35c28b67
Joachim Arrasz <arrasz@synyx.de>
2014-07-27 20:39:29 +0200

    * cleaning

--- src/main/java/org/synyx/samples/spring/boot/SyncController.java
+++ src/main/java/org/synyx/samples/spring/boot/SyncController.java
@@ -1,22 +1,29 @@
 package org.synyx.samples.spring.boot;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.synyx.samples.spring.boot.repo.ProjectRepository;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 
 @RestController
 public class SyncController {
 
-    @RequestMapping(value="/sync", method = POST)
-    public String index(
-        @RequestParam(value="ident", required=true) String ident,
-        @RequestParam(value="content", required=true) String content) {
-        
+    @Autowired
+    ProjectRepository repo;
+
+    @RequestMapping(value = "/sync", method = POST)
+    public String index(@RequestParam(value = "ident", required = true) String ident,
+        @RequestParam(value = "content", required = true) String content) {
+
         System.out.println("Content: " + content);
         System.out.println("Ident: " + ident);
-        
+
         return "Greetings from Spring Boot Sytim Sync! " + ident + " " + content;
     }
-
 }
