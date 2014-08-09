003462b440a7e5617fa6b4eb726b919a0de9d66b
Joachim Arrasz <arrasz@synyx.de>
2014-08-09 09:53:59 +0200

    * initial commit

---
+++ src/main/java/org/synyx/sytim/spring/boot/repo/ProjectRepository.java
@@ -0,0 +1,29 @@
+/*
+ * 27.07.2014
+ */
+
+package org.synyx.sytim.spring.boot.repo;
+
+import org.springframework.data.neo4j.repository.GraphRepository;
+
+import org.synyx.sytim.spring.boot.domain.Project;
+
+
+/**
+ * Enables CRUD Operations on SyTim Projects.
+ *
+ * @author  Joachim Arrasz synyx GmbH & Co. KG
+ */
+
+public interface ProjectRepository extends GraphRepository<Project> {
+
+    /**
+     * Finder for projects by name.
+     *
+     * @param  name  to find
+     * @param  ident  from the person which want to find the project.
+     *
+     * @return  the project with this name.
+     */
+    Project findByNameAndIdent(String name, String ident);
+}
