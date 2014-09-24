444975a164327a9a26cbcdcb01f0103b8a167dd2
Joachim Arrasz <arrasz@synyx.de>
2014-09-24 12:06:12 +0200

    * just added a system out for more ease of debugging

--- src/main/java/org/synyx/sytim/spring/boot/SyncController.java
+++ src/main/java/org/synyx/sytim/spring/boot/SyncController.java
@@ -1,8 +1,9 @@
 package org.synyx.sytim.spring.boot;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,14 +11,17 @@ public class SyncController {
 
     // @Autowired
     // ProjectRepository repo;
-    @RequestMapping(value = "/sync", method = POST, headers = {"Content-type=application/json"})
-    public String index(@RequestParam(value = "ident", required = true) String ident,
-            @RequestParam(value = "content", required = true) String content) {
+    /**
+     * @param projects
+     *
+     * @return
+     */
+    @RequestMapping(value = "/sync", method = RequestMethod.POST, headers = {"Content-type=application/json"})
+    @ResponseBody
+    public String sync(@RequestBody Projects projects) {
 
-        System.out.println("Content: " + content);
+        System.out.println(projects.getProjects().length);
 
-        System.out.println("Ident: " + ident);
-
-        return "Greetings from Spring Boot Sytim Sync! " + ident + " " + content;
+        return "{\"response:ok\"}";
     }
 }
