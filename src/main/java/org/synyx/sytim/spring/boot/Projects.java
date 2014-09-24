d30a79d5eec036e66a81976556abd6c37dd5d9a9
Joachim Arrasz <arrasz@synyx.de>
2014-09-24 12:07:27 +0200

    * initial commit * added timeentry array

---
+++ src/main/java/org/synyx/sytim/spring/boot/Projects.java
@@ -0,0 +1,56 @@
+/*
+ * 14.08.2014
+ */
+package org.synyx.sytim.spring.boot;
+
+import org.synyx.sytim.spring.boot.domain.Project;
+
+/**
+ * @author Joachim Arrasz synyx GmbH & Co. KG
+ */
+public class Projects {
+
+    private String ident;
+
+    private Project[] projects;
+
+    public Projects(String json) {
+
+        System.out.println(json);
+    }
+
+    public Projects() {
+    }
+
+    /**
+     * @return the ident
+     */
+    public String getIdent() {
+
+        return ident;
+    }
+
+    /**
+     * @param ident the ident to set
+     */
+    public void setIdent(String ident) {
+
+        this.ident = ident;
+    }
+
+    /**
+     * @return the projects
+     */
+    public Project[] getProjects() {
+
+        return projects;
+    }
+
+    /**
+     * @param projects the projects to set
+     */
+    public void setProjects(Project[] projects) {
+
+        this.projects = projects;
+    }
+}
