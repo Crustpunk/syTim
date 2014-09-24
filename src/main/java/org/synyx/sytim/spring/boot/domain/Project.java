ea4446f49e3afcabd953de82228d3e601124b8f2
Joachim Arrasz <arrasz@synyx.de>
2014-09-24 12:07:10 +0200

    * change from Set to Array of projects

--- src/main/java/org/synyx/sytim/spring/boot/domain/Project.java
+++ src/main/java/org/synyx/sytim/spring/boot/domain/Project.java
@@ -4,20 +4,15 @@
 package org.synyx.sytim.spring.boot.domain;
 
 import org.neo4j.graphdb.Direction;
-
 import org.springframework.data.neo4j.annotation.Fetch;
 import org.springframework.data.neo4j.annotation.GraphId;
 import org.springframework.data.neo4j.annotation.NodeEntity;
 import org.springframework.data.neo4j.annotation.RelatedTo;
 
-import java.util.HashSet;
-import java.util.Set;
-
-
 /**
  * Represents a SyTim Project.
  *
- * @author  Joachim Arrasz synyx GmbH & Co. KG
+ * @author Joachim Arrasz synyx GmbH & Co. KG
  */
 @NodeEntity
 public class Project {
@@ -25,19 +20,21 @@ public class Project {
     @GraphId
     public int graphId;
 
-    public String name;
-    public String ident;
+    public String project;
 
+//    @RelatedTo(type = "TIMEENTRY", direction = Direction.OUTGOING)
+//    @Fetch
+//    public Set<TimeEntry> timeentries;
     @RelatedTo(type = "TIMEENTRY", direction = Direction.OUTGOING)
     @Fetch
-    public Set<TimeEntry> timeentries;
+    public TimeEntry[] timeentries;
 
-    public void addTimeEntry(TimeEntry entry) {
-
-        if (timeentries == null) {
-            timeentries = new HashSet<TimeEntry>();
-        }
-
-        timeentries.add(entry);
-    }
+//    public void addTimeEntry(TimeEntry entry) {
+//
+//        if (timeentries == null) {
+//            timeentries = new HashSet<TimeEntry>();
+//        }
+//
+//        timeentries.add(entry);
+//    }
 }
