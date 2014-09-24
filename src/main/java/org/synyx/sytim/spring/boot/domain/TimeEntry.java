f514a29995f687a0d19b8e8631a656c00ed81683
Joachim Arrasz <arrasz@synyx.de>
2014-09-24 12:08:27 +0200

    * added jackson mapping annotations for date handling

--- src/main/java/org/synyx/sytim/spring/boot/domain/TimeEntry.java
+++ src/main/java/org/synyx/sytim/spring/boot/domain/TimeEntry.java
@@ -3,6 +3,7 @@
  */
 package org.synyx.sytim.spring.boot.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.Date;
 import org.springframework.data.neo4j.annotation.GraphId;
 import org.springframework.data.neo4j.annotation.NodeEntity;
@@ -19,7 +20,13 @@ public class TimeEntry {
     public int graphId;
 
     public String desc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm", timezone = "GMT")
     public Date from;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm", timezone = "GMT")
     public Date to;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "GMT")
     public Date when;
 }
